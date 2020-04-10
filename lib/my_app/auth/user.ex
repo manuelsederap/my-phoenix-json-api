2119037ac05c09d7c722ec0c6d6a9cbd39eb330a
manuel_paredes <manuelsederap@gmail.com>
2020-04-10 13:20:18 +0800

    initial-commit

---
+++ lib/my_app/auth/user.ex
@@ -0,0 +1,32 @@
+defmodule MyApp.Auth.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "users" do
+    field :email, :string
+    field :is_active, :boolean, default: false
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    # Add support for microseconds at the language level
+    # for this specific schema
+    timestamps(type: :utc_datetime_usec)
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :is_active, :password])
+    |> validate_required([:email, :is_active, :password])
+    |> unique_constraint(:email)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, password_hash: Bcrypt.hash_pwd_salt(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
